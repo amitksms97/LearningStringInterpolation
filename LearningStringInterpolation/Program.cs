b7c0cf407c40698d316ae4bd40bbd7c377d3fc31
amitksms97 <amitksms97@gmail.com>
2021-05-01 20:02:46 +0530

    Add project files.

---
+++ LearningStringInterpolation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LearningStringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new { x = 1, y = 2 };
+            Console.WriteLine("x" + point.x + "y" + point.y); // String Concatination
+            Console.WriteLine("x {0} y {1}" + point.x , point.y); // String Formatting
+            Console.WriteLine($"x {point.x} y {point.y}"); // String Formatting
+        }
+    }
+}
